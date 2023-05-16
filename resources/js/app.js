c422a4e52a821665da6f9f3586f6d0bb0040f493
meleadr <meledina.adriano@gmail.com>
2023-05-16 19:17:08 +0200

    Architecture du projet et création des routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import("./bootstrap");
+
+import { createApp } from "vue";
+
+import App from "./components/App.vue";
+
+import router from "./router";
+
+// 3. On monte l'application Vue sur l'élément #app
+createApp(App).use(router).mount("#app");
---
+++ resources/js/router/index.js
@@ -0,0 +1,56 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Admin
+import Admin from "../components/pages/Admin.vue";
+import Presentation from "../components/pages/Presentation.vue";
+import Login from "../components/pages/Login.vue";
+import Chalet from "../components/pages/Chalet.vue";
+
+// Pages
+
+// NotFound
+import NotFound from "../components/pages/NotFound.vue";
+
+const routes = [
+    {
+        path: "/",
+        Name: "Presentation",
+        component: Presentation,
+    },
+    {
+        path: "/chalet",
+        Name: "Chalet",
+        component: Chalet,
+    },
+    {
+        path: "/admin",
+        Name: "Admin",
+        component: Admin,
+        meta: {
+            requiresAuth: true,
+        },
+    },
+    {
+        path: "/admin/login",
+        Name: "Login",
+        component: Login,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        Name: "NotFound",
+        component: NotFound,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+router.beforeEach((to, from) => {
+    if (to.meta.requiresAuth && !localStorage.getItem("token")) {
+        return "/admin/login";
+    }
+});
+
+export default router;

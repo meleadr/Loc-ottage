d19d7fd5bd5b3f74be8b41d21caae5ead13127b2
meleadr <meledina.adriano@gmail.com>
2023-05-25 18:25:27 +0200

    Correction routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -46,7 +46,7 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from) => {
-    if (to.meta.requiresAuth && !localStorage.getItem("token")) {
+    if (to.meta.requiresAuth && !sessionStorage.getItem("token")) {
         return "/admin/login";
     }
 });

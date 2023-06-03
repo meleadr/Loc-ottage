d36db568fd326426923fa00cc35d01056785c798
Alexandre <102592431+Foxenz@users.noreply.github.com>
2023-06-03 18:37:17 +0200

    Icon cards

--- resources/js/components/CottageCard.vue
+++ resources/js/components/CottageCard.vue
@@ -3,10 +3,10 @@
         <div class="features">
             <table class="table_info">
                 <tr>
-                    <th>Surface</th>
-                    <th>Nombre de personnes</th>
-                    <th>Nombre de chambres</th>
-                    <th>Prix</th>
+                    <th><i class='bx bx-area icon'></i></th>
+                    <th><i class='bx bx-user icon'></i></th>
+                    <th><i class='bx bx-bed icon' ></i></th>
+                    <th><i class='bx bx-purchase-tag-alt icon' ></i></th>
                 </tr>
 
                 <tr>
@@ -47,12 +47,17 @@
     .table_info {
         margin: 2rem 0;
         width: 100%;
+        border-radius: 20px;
         border-collapse: collapse;
         text-align: center;
         background-color: grey;
 
         th, td {
             padding: 1rem;
+        }
+
+        .icon {
+            font-size: 2rem;
         }
     }
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
-        <title>Loc'ottage</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Loc'ottage</title>
 
-		@vite('resources/sass/app.scss')
-    </head>
-    <body>
-      <div id="app"></div>
-	  @vite('resources/js/app.js')
-    </body>
+    @vite('resources/sass/app.scss')
+</head>
+
+<body>
+    <div id="app"></div>
+    @vite('resources/js/app.js')
+</body>
+
 </html>

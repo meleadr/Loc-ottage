049cf1d72b5f8c03177e2147ec2a5025fb4dee88
meleadr <meledina.adriano@gmail.com>
2023-06-04 17:50:51 +0200

    Seeders pour 50 bookings

--- database/seeders/BookingsTableSeeder.php
+++ database/seeders/BookingsTableSeeder.php
@@ -30,52 +30,6 @@ class BookingsTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        DB::table('bookings')->insert([
-            'start_date' => '2023-11-23',
-            'end_date' => '2023-11-24',
-            'price' => 3000,
-            'name' => 'Marie',
-            'surname' => 'Jane',
-            'email' => 'marie.jane@example.com',
-            'phone' => '1234567890',
-            'persons' => 8,
-            'cottage_id' => 3,  // assuming you have a cottage with this id
-            'option_id' => 3,   // assuming you have an option with this id
-            'status_id' => 1,   // assuming you have a status with this id
-            'created_at' => '2023-06-01',
-            'updated_at' => now(),
-        ]);
-
-        DB::table('bookings')->insert([
-            'start_date' => '2023-08-21',
-            'end_date' => '2023-08-24',
-            'price' => 400,
-            'name' => 'Sarah',
-            'surname' => 'Croche',
-            'email' => 'sarah.croche@example.com',
-            'phone' => '1234567890',
-            'persons' => 2,
-            'cottage_id' => 2,  // assuming you have a cottage with this id
-            'option_id' => 4,   // assuming you have an option with this id
-            'status_id' => 1,   // assuming you have a status with this id
-            'created_at' => '2023-06-02',
-            'updated_at' => now(),
-        ]);
-
-        DB::table('bookings')->insert([
-            'start_date' => '2023-12-22',
-            'end_date' => '2023-12-24',
-            'price' => 4334,
-            'name' => 'Alex',
-            'surname' => 'Térieur',
-            'email' => 'alex.térieur@example.com',
-            'phone' => '1234567890',
-            'persons' => 4,
-            'cottage_id' => 1,  // assuming you have a cottage with this id
-            'option_id' => 1,   // assuming you have an option with this id
-            'status_id' => 1,   // assuming you have a status with this id
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+		\App\Models\Booking::factory()->count(50)->create();
     }
 }

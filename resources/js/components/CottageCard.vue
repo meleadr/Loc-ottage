dc65780dcea154132f84fc575c022de9187fdb45
meleadr <meledina.adriano@gmail.com>
2023-06-04 08:50:43 +0200

    Probleme de route pour le chalet

--- resources/js/components/CottageCard.vue
+++ resources/js/components/CottageCard.vue
@@ -1,35 +1,39 @@
 <template>
     <div class="card">
-        <div class="image" :class="{ 'image-left': id % 2 === 0, 'image-right': id % 2 === 1 }">
-            <img :src="image_url" alt="">
+        <div
+            class="image"
+            :class="{ 'image-left': id % 2 === 0, 'image-right': id % 2 === 1 }"
+        >
+            <img :src="image_url" :alt="name" />
         </div>
 
-        <div class="info" :class="{ 'info-left': id % 2 === 0, 'info-right': id % 2 === 1 }">
+        <div
+            class="info"
+            :class="{ 'info-left': id % 2 === 0, 'info-right': id % 2 === 1 }"
+        >
             <h2>{{ name }}</h2>
             <p>{{ description }}</p>
 
             <CottageInfo
                 :area="area"
-                :persons="persons" 
+                :persons="persons"
                 :bedrooms="bedrooms"
                 :price="price"
             />
 
             <div class="button_container">
-                <a class="button" href="Chalet">
-                    Voir plus
-                </a>
+                <a class="button" :href="lien"> Voir plus </a>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-// Import des composants
 import CottageInfo from "./CottageInfo.vue";
+import { defineProps, computed } from "vue";
 
 const props = defineProps({
-    id:{
+    id: {
         type: Number,
         required: true,
     },
@@ -62,8 +66,12 @@ const props = defineProps({
         required: true,
     },
 });
+
+const lien = computed(() => {
+    return "/chalet/" + props.id;
+});
 </script>
-  
+
 <style scoped lang="scss">
 .card {
     display: flex;
@@ -80,18 +88,18 @@ const props = defineProps({
     padding: 0 2rem;
     color: orange;
 
-&-left {
-    order: 2;
-}
+    &-left {
+        order: 2;
+    }
 
-&-right {
-    order: 1;
-}
+    &-right {
+        order: 1;
+    }
 
-.button_container {
-    margin: 2rem 0;
-    text-align: center;
-}
+    .button_container {
+        margin: 2rem 0;
+        text-align: center;
+    }
 }
 
 .image {
@@ -101,18 +109,18 @@ const props = defineProps({
     justify-content: center;
     align-items: center;
 
-&-left {
-    order: 1;
-}
+    &-left {
+        order: 1;
+    }
 
-&-right {
-    order: 2;
-}
+    &-right {
+        order: 2;
+    }
 
-img {
-    width: 100%;
-    height: auto;
-    border-radius: 20px;
-}
+    img {
+        width: 100%;
+        height: auto;
+        border-radius: 20px;
+    }
 }
 </style>
